2566b2007d64f351a5c27449dd1a662a4a952700
H3c <h3clikejava@gmail.com>
2016-06-03 10:52:20 +0800

    增加三个异步处理的方法

--- library/src/main/java/com/h3c/afastproject/utils/TaskUtils.java
+++ library/src/main/java/com/h3c/afastproject/utils/TaskUtils.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 import rx.subjects.PublishSubject;
 
 /**
@@ -13,10 +14,27 @@ import rx.subjects.PublishSubject;
 
 public class TaskUtils {
     public static void timer(long time, TimeUnit timeUnit, Action1 action) {
+        if(action == null || timeUnit == null) return;
         PublishSubject.create().timer(time, timeUnit).subscribe(action);
     }
 
     public static void runOnUI(Action0 action) {
+        if(action == null) return;
         AndroidSchedulers.mainThread().createWorker().schedule(action);
+    }
+
+    public static void runIO(Action0 action) {
+        if(action == null) return;
+        Schedulers.io().createWorker().schedule(action);
+    }
+
+    public static void runComputation(Action0 action) {
+        if(action == null) return;
+        Schedulers.computation().createWorker().schedule(action);
+    }
+
+    public static void runNewThread(Action0 action) {
+        if(action == null) return;
+        Schedulers.newThread().createWorker().schedule(action);
     }
 }
